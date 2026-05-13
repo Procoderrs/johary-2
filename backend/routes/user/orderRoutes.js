81302f15cfef5db43f37fbdbe3b4e750b8e335cf
Zeenat <zeenat.zeni0112@gmail.com>
2026-05-13 16:44:11 +0500

    commit

--- backend/routes/user/orderRoutes.js
+++ backend/routes/user/orderRoutes.js
@@ -11,9 +11,9 @@ import { protect } from "../../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/webhook", express.raw({ type: "application/json" }), stripeWebhook);
-router.post("/create", createOrder);
-router.get("/my-orders", protect, getMyOrders); 
-router.get("/:id", getOrderById);               
+router.post("/create", protect, createOrder); 
+router.get("/my-orders", protect, getMyOrders);
+router.get("/:id", getOrderById);
 router.post("/:id/pay", payOrder);
 
 export default router;
