cee46aab53505cf496057957aac310fd92b4a550
Zeenat <zeenat.zeni0112@gmail.com>
2026-05-15 15:00:33 +0500

    commit

--- frontend/src/data/shop.js
+++ frontend/src/data/shop.js
@@ -2,26 +2,25 @@ export const shop = [
   {
     heading: "Product Types",
     items: [
-      { label: "Simple Product", link: "/product/simple" },
-      { label: "Grouped Product", link: "/product/grouped" },
-            { label: "Variable Product", link: "/product/variable",badge: "NEW" },
-      { label: "External Product", link: "/product/external",  },
-      { label: "Sale Product", link: "/product/downloadable",badge: "SALE" },
-      { label: "Upsell Products", link: "/product/virtual" },
-      { label: "Cross-Sell Product", link: "/product/crosee-sell", },
+      { label: "Simple Product", link: "/shop" },
+      { label: "Grouped Product", link: "/shop" },
+      { label: "Variable Product", link: "/shop", badge: "NEW" },
+      { label: "External Product", link: "/shop" },
+      { label: "Sale Product", link: "/shop", badge: "SALE" },
+      { label: "Upsell Products", link: "/shop" },
+      { label: "Cross-Sell Product", link: "/shop" },
     ],
   },
 
   {
     heading: "WooCommerce Pages",
     items: [
-            { label: "Shop", link: "/shop", },
-
+      { label: "Shop", link: "/shop" },
       { label: "Shopping Cart", link: "/cart" },
       { label: "Checkout Page", link: "/checkout" },
       { label: "My Account", link: "/account" },
-      { label: "Shop Ajax Filter", link: "/wishlist",badge: "HOT"  },
-      { label: "Product Category", link: "/order-tracking" },
+      { label: "Shop Ajax Filter", link: "/shop", badge: "HOT" },
+      { label: "Product Category", link: "/shop" },
       { label: "Privacy Policy", link: "/privacy" },
     ],
   },
@@ -29,13 +28,13 @@ export const shop = [
   {
     heading: "Product Features",
     items: [
-      { label: "Stock Progress Bar", link: "/sale" },
-      { label: "Color/Image Swatches", link: "/featured" },
-      { label: "Size Guide Table", link: "/best-selling",badge: "POPULAR" },
-      { label: "Custom Tab", link: "/top-rated",  },
-      { label: "Countdown Timer", link: "/new" },
-      { label: "Product Video", link: "/trending",badge: "FEATURED" },
-      { label: "Product Brand", link: "/recent" },
+      { label: "Stock Progress Bar", link: "/shop" },
+      { label: "Color/Image Swatches", link: "/shop" },
+      { label: "Size Guide Table", link: "/shop", badge: "POPULAR" },
+      { label: "Custom Tab", link: "/shop" },
+      { label: "Countdown Timer", link: "/shop" },
+      { label: "Product Video", link: "/shop", badge: "FEATURED" },
+      { label: "Product Brand", link: "/shop" },
     ],
   },
 ];
